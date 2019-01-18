34bd69d5d42ab859faf03570ab3f77397fc6d520
Vasile Drumea <drumeavasilexd@gmail.com>
2019-01-18 17:23:28 +0200

    Add Firebase authentication

--- app/src/main/java/com/example/vasile/telemedicine/HomeActivity.java
+++ app/src/main/java/com/example/vasile/telemedicine/HomeActivity.java
@@ -2,20 +2,44 @@ package com.example.vasile.telemedicine;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeActivity extends AppCompatActivity {
 
     private Button requestButton;
+    // private Button signoutButton;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // initialize FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null) {
+                    startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                }
+            }
+        };
+
+        // initialize Request button
         requestButton = (Button) findViewById(R.id.request_button);
         requestButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +47,15 @@ public class HomeActivity extends AppCompatActivity {
                 request(v);
             }
         });
+
+        // initialize Signout button
+        // signoutButton = (Button) findViewById(R.id.signout_button);
+        // signoutButton.setOnClickListener(new View.OnClickListener() {
+        //     @Override
+        //     public void onClick(View v) {
+        //         mAuth.signOut();
+        //     }
+        // });
     }
 
     public void request(View view) {
--- app/src/main/java/com/example/vasile/telemedicine/MainActivity.java
+++ app/src/main/java/com/example/vasile/telemedicine/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button signupButton;
